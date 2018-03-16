b9d80d4b6656ebecec77941f526e8eb3550e7940
Eurynomous666 <regmi.sagar244@gmail.com>
2018-03-16 18:17:41 +0545

    Third Commit
    Image has been displayed

--- Fractal/Fractal/HSB.cs
+++ Fractal/Fractal/HSB.cs
@@ -15,70 +15,53 @@ namespace Fractal
         }
         public void fromHSB(float h, float s, float b)
         {
-            float red = b;
-            float green = b;
-            float blue = b;
-            if (s != 0)
+            if (s == 0)
             {
-                float max = b;
-                float dif = b * s / 255f;
-                float min = b - dif;
-
-                float h2 = h * 360f / 255f;
-
-                if (h2 < 60f)
+                rChan = gChan = bChan = (int)(b * 255.0f + 0.5f);
+            }
+            else
+            {
+                h = (h - (float)Math.Floor(h)) * 6.0f;
+                float f = h - (float)Math.Floor(h);
+                float p = b * (1.0f - s);
+                float q = b * (1.0f - s * f);
+                float t = b * (1.0f - (s * (1.0f - f)));
+                switch ((int)h)
                 {
-                    red = max;
-                    green = h2 * dif / 60f + min;
-                    blue = min;
-                }
-                else if (h2 < 120f)
-                {
-                    red = -(h2 - 120f) * dif / 60f + min;
-                    green = max;
-                    blue = min;
-                }
-                else if (h2 < 180f)
-                {
-                    red = min;
-                    green = max;
-                    blue = (h2 - 120f) * dif / 60f + min;
-                }
-                else if (h2 < 240f)
-                {
-                    red = min;
-                    green = -(h2 - 240f) * dif / 60f + min;
-                    blue = max;
-                }
-                else if (h2 < 300f)
-                {
-                    red = (h2 - 240f) * dif / 60f + min;
-                    green = min;
-                    blue = max;
-                }
-                else if (h2 <= 360f)
-                {
-                    red = max;
-                    green = min;
-                    blue = -(h2 - 360f) * dif / 60 + min;
-                }
-                else
-                {
-                    red = 0;
-                    green = 0;
-                    blue = 0;
+                    case 0:
+                        rChan = (int)(b * 255.0f + 0.5f);
+                        gChan = (int)(t * 255.0f + 0.5f);
+                        bChan = (int)(p * 255.0f + 0.5f);
+                        break;
+                    case 1:
+                        rChan = (int)(q * 255.0f + 0.5f);
+                        gChan = (int)(b * 255.0f + 0.5f);
+                        bChan = (int)(p * 255.0f + 0.5f);
+                        break;
+                    case 2:
+                        rChan = (int)(p * 255.0f + 0.5f);
+                        gChan = (int)(b * 255.0f + 0.5f);
+                        bChan = (int)(t * 255.0f + 0.5f);
+                        break;
+                    case 3:
+                        rChan = (int)(p * 255.0f + 0.5f);
+                        gChan = (int)(q * 255.0f + 0.5f);
+                        bChan = (int)(b * 255.0f + 0.5f);
+                        break;
+                    case 4:
+                        rChan = (int)(t * 255.0f + 0.5f);
+                        gChan = (int)(p * 255.0f + 0.5f);
+                        bChan = (int)(b * 255.0f + 0.5f);
+                        break;
+                    case 5:
+                        rChan = (int)(b * 255.0f + 0.5f);
+                        gChan = (int)(p * 255.0f + 0.5f);
+                        bChan = (int)(q * 255.0f + 0.5f);
+                        break;
                 }
             }
-
-            rChan = (float)Math.Round(Math.Min(Math.Max(red, 0f), 255));
-            gChan = (float)Math.Round(Math.Min(Math.Max(green, 0), 255));
-            bChan = (float)Math.Round(Math.Min(Math.Max(blue, 0), 255));
-
         }
-
     }
+
+
 }
-    
-
-
-
